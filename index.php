c44ad7feba15893d89116cf7eb1414bf25bdc81f
Md. Alamin Miah <102720016+alaminces@users.noreply.github.com>
2022-04-08 01:15:22 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Fulkuri Computer Training Center Login Page</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+   
+    
+  </head>
+  <body>
+    <div class="bg-section">
+        <div class="container">
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-6 login-area p-5">
+                <div class="row mt-3">
+                    <div class="col-md-4 st-text">“বেকারত্ব দূর করতে যাই কম্পিউটার শিখতে”</div>
+                    <div class="col-md-4">
+                        <img src="img/logo.png" width="100" alt="">
+                    </div>
+                    <div class="col-md-4 st-text">“কম্পিউটার প্রশিক্ষণ অতন্ত্য জরুরী”</div>
+                </div>
+                <div class="row fctc-name p-3 mt-3">
+                    <h1>ফুলকুড়ি কম্পিউটার ট্রেনিং সেন্টার</h1>
+                    <cite>(এফ সি টি সি)</cite>
+                    <p class="text-center p-3">একটি অত্যাধুনিক ও অনন্য মানের প্রতিষ্ঠান। অর্থ নয় সেবাই আমাদের মুল লক্ষ্য </p>
+                </div>
+
+                <div class="row">
+                <form>
+                    <div class="row mb-3">
+                        <div class="col-3">
+                            <label for="inputUsername" class="col-sm-2 col-form-label">ইউজারনেম</label>
+                        </div>
+                        <div class="col-9">
+                            <div class="input-group flex-nowrap">
+                                <span class="input-group-text" id="addon-wrapping"><i class="fa fa-user"></i></span>
+                                <input type="text" class="form-control" id="inputUsername" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-3 mb-3">
+                            <label for="inputPassword3" class="col-sm-2 col-form-label">পাসওয়ার্ড</label>    
+                        </div>
+                        <div class="col-9">
+                             <div class="input-group flex-nowrap">
+                                <span class="input-group-text" id="addon-wrapping"><i class="fa fa-lock"></i></span>
+                                <input type="text" class="form-control" id="inputPassword3" placeholder="Password" aria-label="Password" aria-describedby="addon-wrapping">
+                            </div>
+                        </div>
+                    </div>
+                   
+                       <button type="submit" class="btn btn-primary" style="float:right" name="singn_in"><i class="fas fa-sign-in-alt"></i> সাইন ইন</button> 
+                    
+                </form>
+
+                <p class="text-center reg mt-5">Already have an account? <a href="#">Registration here </a></p>
+
+                    
+
+                  
+
+
+
+
+            <div class="col-md-3"></div>
+        </div>
+        
+    </div>  
+    </div>
+    
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
